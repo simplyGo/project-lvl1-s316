366a22b1e490d362949c3fbbf32fc97b3524024f
simply-smile <mark.cardinal@yandex.ru>
2018-08-01 23:22:59 +0400

    second step

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import askName from '../index.js';
 
 console.log('Welcome to the Brain Games!');
+
+askName();
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+function askName() {
+  console.log('May I have your name?');
+  const actualName = readlineSync.question('Your name: ');
+
+  console.log(`Hello, ${actualName}`);
+};
+
+export default askName;
