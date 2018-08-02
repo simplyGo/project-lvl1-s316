e983ff7cdcb7ab6da869f752b6308817f25db3d7
simply-smile <mark.cardinal@yandex.ru>
2018-08-02 21:13:20 +0400

    changed question gcd

--- src/games.js
+++ src/games.js
@@ -91,6 +91,7 @@ const runGcdGame = (user, times) => {
     const a = getRandom(0, 10) * multiply;
     const b = getRandom(0, 10) * multiply;
     const rightAnswer = getGCD(a, b);
+    console.log(`Question: ${a} ${b}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (!isCorrect(userName, Number(userAnswer), rightAnswer)) return;
   }
